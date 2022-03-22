88d4b69672cf32c7ff50cddb5ae38718d49abed2
呉真 <kuretru@gmail.com>
2022-03-22 20:19:56 +0800

    新增(API)：OAuth权限管理接口

---
+++ Gemini-OAuth2-Api/gemini-oauth2-api/src/main/java/com/kuretru/web/gemini/service/OAuthPermissionService.java
@@ -0,0 +1,12 @@
+package com.kuretru.web.gemini.service;
+
+import com.kuretru.api.common.service.BaseService;
+import com.kuretru.web.gemini.entity.query.OAuthPermissionQuery;
+import com.kuretru.web.gemini.entity.transfer.OAuthPermissionDTO;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+public interface OAuthPermissionService extends BaseService<OAuthPermissionDTO, OAuthPermissionQuery> {
+
+}
