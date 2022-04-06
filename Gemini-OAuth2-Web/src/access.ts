a20f3f39a568c70891ed9dc1e4262bff5586e12e
呉真 <kuretru@gmail.com>
2022-04-06 14:46:56 +0800

    新增(Web)：页面权限配置

--- Gemini-OAuth2-Web/src/access.ts
+++ Gemini-OAuth2-Web/src/access.ts
@@ -1,9 +1,9 @@
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
-export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
+export default function access(initialState: { currentUser?: API.User.UserDTO | undefined }) {
   const { currentUser } = initialState || {};
   return {
-    canAdmin: currentUser && currentUser.access === 'admin',
+    isAdmin: currentUser && currentUser.isAdmin,
   };
 }
