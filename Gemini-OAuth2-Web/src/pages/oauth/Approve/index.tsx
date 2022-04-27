3fc87ac547de1a191c414e29b443e7da274a568a
呉真 <kuretru@gmail.com>
2022-04-27 20:40:13 +0800

    新增：允许的内容

--- Gemini-OAuth2-Web/src/pages/oauth/Approve/index.tsx
+++ Gemini-OAuth2-Web/src/pages/oauth/Approve/index.tsx
@@ -1,16 +1,64 @@
 import React from 'react';
 import { GridContent } from '@ant-design/pro-layout';
 import ProCard from '@ant-design/pro-card';
-import { Button } from 'antd';
-import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import { Avatar, Button, List, message, Tooltip } from 'antd';
+import {
+  ArrowRightOutlined,
+  CheckCircleOutlined,
+  CloseCircleOutlined,
+  MailOutlined,
+  PhoneOutlined,
+  SmileOutlined,
+} from '@ant-design/icons';
 import { approve } from '@/services/gemini-oauth2/oauth2';
+import OAuthApplicationService from '@/services/gemini-oauth2/oauth/application';
 import { getRequestParams } from '@/utils/request-utils';
+import styles from './index.less';
 
 interface OAuthApproveProps {}
 
-interface OAuthApproveState {}
+interface OAuthApproveState {
+  application: API.OAuth.OAuthApplicationDTO;
+}
 
 class OAuthApprove extends React.Component<OAuthApproveProps, OAuthApproveState> {
+  oAuthApplicationService: OAuthApplicationService = new OAuthApplicationService();
+  permissions: API.OAuth.OAuthPermissionDTO[] = [
+    {
+      permission: '',
+      avatar: <SmileOutlined />,
+      title: '昵称',
+      description: '昵称和头像',
+    },
+    {
+      permission: 'email',
+      avatar: <MailOutlined />,
+      title: '电子邮箱',
+      description: '电子邮箱地址',
+    },
+    {
+      permission: 'mobile',
+      avatar: <PhoneOutlined />,
+      title: '手机号码',
+      description: '手机号码',
+    },
+  ];
+
+  constructor(props: OAuthApproveProps) {
+    super(props);
+    this.getApplication();
+  }
+
+  getApplication = async () => {
+    const requestParams = getRequestParams();
+    if (!requestParams.application_id) {
+      message.error('OAuth应用ID不存在');
+      return;
+    }
+    const application = await this.oAuthApplicationService.get(requestParams.application_id);
+    this.setState({ application: application.data });
+  };
+
   userApprove = async (action: string) => {
     const requestParams = getRequestParams();
     const params: API.OAuth2.OAuth2ApproveRequestDTO = {
@@ -42,9 +90,39 @@ class OAuthApprove extends React.Component<OAuthApproveProps, OAuthApproveState>
   render() {
     return (
       <GridContent>
-        <ProCard title="OAuth2身份认证" actions={this.actionbutton}>
-          您是否允许访问：
-        </ProCard>
+        <div className={styles.container}>
+          <ProCard title="OAuth2身份认证" actions={this.actionbutton} style={{ width: '64em' }}>
+            <div className={styles.avatar}>
+              <Avatar size={96} src={this.state?.application.avatar} />
+              <ArrowRightOutlined className={styles.arrow} />
+              <Avatar size={96} src={this.state?.application.avatar} />
+            </div>
+            您是否允许
+            <Tooltip placement={'bottomLeft'} title={this.state?.application.description}>
+              <Button type="link">{this.state?.application.name}</Button>
+            </Tooltip>
+            进行以下操作：
+            <List
+              bordered
+              dataSource={this.permissions}
+              itemLayout={'horizontal'}
+              renderItem={(item) => (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar={<Avatar size={40} icon={item.avatar} />}
+                    title={<strong>{item.title}</strong>}
+                    description={
+                      <>
+                        <strong>读取{item.permission.endsWith('_w') ? '/修改' : ''}</strong>您的
+                        {item.description}
+                      </>
+                    }
+                  />
+                </List.Item>
+              )}
+            />
+          </ProCard>
+        </div>
       </GridContent>
     );
   }
