fb63275c75aecce3c910b39a9d4035a4dcb5f8bb
呉真 <kuretru@gmail.com>
2022-11-14 21:36:26 +0800

    新增(Web)：权限标签组件

---
+++ Gemini-OAuth2-Web/src/pages/oauth/Permission/components/PermissionLabel.tsx
@@ -0,0 +1,18 @@
+import { Space, Tag } from "antd";
+import React from "react";
+
+export type PermissionLabelProps = {
+    type: string;
+    name: string;
+};
+
+const PermissionLabel: React.FC<PermissionLabelProps> = (props) => {
+    return (
+        <Space>
+            <Tag color={props.type === "读取" ? "green" : "orange"}>
+                {props.type} {props.name}
+            </Tag>
+        </Space>
+    );
+}
+export default PermissionLabel;
