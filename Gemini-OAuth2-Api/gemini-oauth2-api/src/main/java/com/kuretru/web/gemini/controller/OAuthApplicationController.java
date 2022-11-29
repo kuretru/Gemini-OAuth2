20d6623777486fbb7a772ec815b30bee956b113a
呉真 <kuretru@gmail.com>
2022-11-29 18:44:13 +0800

    修复(API)：普通用户无法获得应用信息的问题

--- Gemini-OAuth2-Api/gemini-oauth2-api/src/main/java/com/kuretru/web/gemini/controller/OAuthApplicationController.java
+++ Gemini-OAuth2-Api/gemini-oauth2-api/src/main/java/com/kuretru/web/gemini/controller/OAuthApplicationController.java
@@ -1,13 +1,23 @@
 package com.kuretru.web.gemini.controller;
 
 import com.kuretru.microservices.authentication.annotaion.RequireAuthorization;
+import com.kuretru.microservices.common.constant.EmptyConstants;
+import com.kuretru.microservices.web.constant.code.UserErrorCodes;
 import com.kuretru.microservices.web.controller.BaseRestController;
+import com.kuretru.microservices.web.entity.ApiResponse;
+import com.kuretru.microservices.web.exception.ServiceException;
 import com.kuretru.web.gemini.entity.query.OAuthApplicationQuery;
 import com.kuretru.web.gemini.entity.transfer.OAuthApplicationDTO;
 import com.kuretru.web.gemini.service.OAuthApplicationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
@@ -20,6 +30,13 @@ public class OAuthApplicationController extends BaseRestController<OAuthApplicat
     @Autowired
     public OAuthApplicationController(OAuthApplicationService service) {
         super(service);
+    }
+
+    @GetMapping("/{id}")
+    @RequireAuthorization
+    @Override
+    public ApiResponse<OAuthApplicationDTO> get(@PathVariable("id") UUID id) throws ServiceException {
+        return super.get(id);
     }
 
 }
