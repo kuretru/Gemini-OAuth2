092fcf0118aa73073350ff0b4a56861da6940423
呉真 <kuretru@gmail.com>
2022-11-29 14:31:12 +0800

    Revert "新增(API)：查看已授权的应用接口"
    This reverts commit 00e1c35af3c25ccab8cb919c61d44eaa015863ec.

--- Gemini-OAuth2-Api/gemini-oauth2-api/src/main/java/com/kuretru/web/gemini/controller/OAuthPermissionController.java
+++ Gemini-OAuth2-Api/gemini-oauth2-api/src/main/java/com/kuretru/web/gemini/controller/OAuthPermissionController.java
@@ -14,10 +14,11 @@ import com.kuretru.web.gemini.entity.query.OAuthPermissionQuery;
 import com.kuretru.web.gemini.entity.view.OAuthPermissionVO;
 import com.kuretru.web.gemini.service.OAuthPermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.UUID;
 
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
@@ -36,36 +37,27 @@ public class OAuthPermissionController extends BaseController {
 
     @GetMapping
     public ApiResponse<?> list(PaginationQuery paginationQuery, OAuthPermissionQuery query) throws ServiceException {
-        if (PaginationQuery.isNull(paginationQuery)) {
+        if (paginationQuery == null || paginationQuery.getCurrent() == null || paginationQuery.getPageSize() == null) {
             throw ServiceException.build(UserErrorCodes.REQUEST_PARAMETER_ERROR, "分页查询参数不存在");
         } else if (UuidUtils.isNotEmpty(query.getApplicationId()) && !AccessTokenContext.hasRoles(RoleConstants.ADMIN)) {
             // 只有管理员可以根据应用查找权限
             throw ServiceException.build(UserErrorCodes.ACCESS_PERMISSION_ERROR, "只有管理员具有根据应用查找的权限");
         } else if (UuidUtils.isNotEmpty(query.getUserId()) && !query.getUserId().equals(AccessTokenContext.getUserId())) {
-            // 查找自己的权限时，横向鉴权
+            // 横向鉴权
             throw ServiceException.build(UserErrorCodes.ACCESS_PERMISSION_ERROR, "请勿操作别人的数据");
-        } else if (UuidUtils.isEmpty(query.getUserId()) && UuidUtils.isEmpty(query.getApplicationId())) {
-            // 补充查询自己的查询参数
+        } else if (UuidUtils.isEmpty(query.getUserId()) && !AccessTokenContext.hasRoles(RoleConstants.ADMIN)) {
+            // 只能查询自己的权限，除非有管理员权限
             query.setUserId(AccessTokenContext.getUserId());
         }
 
         PaginationResponse<OAuthPermissionVO> result = service.listVo(paginationQuery, query);
-        if (result.getList() == null) {
+        if (null == result.getList()) {
             result.setList(new ArrayList<>());
         }
         if (result.getList().isEmpty()) {
             return ApiResponse.notFound(result);
         }
         return ApiResponse.success(result);
-    }
-
-    @DeleteMapping("/{id}")
-    public ApiResponse<String> remove(@PathVariable("id") UUID id) throws ServiceException {
-        if (UuidUtils.isEmpty(id)) {
-            throw ServiceException.build(UserErrorCodes.REQUEST_PARAMETER_ERROR, "未指定ID或ID错误");
-        }
-        service.remove(id);
-        return ApiResponse.removed("资源已删除");
     }
 
 }
