ef1786b894d6fd505d32f148c546292e8c38e01b
呉真 <kuretru@gmail.com>
2022-11-29 14:31:12 +0800

    Revert "新增(Web)：权限标签组件"
    This reverts commit fb63275c75aecce3c910b39a9d4035a4dcb5f8bb.

--- Gemini-OAuth2-Web/src/pages/oauth/Permission/index.tsx
+++ Gemini-OAuth2-Web/src/pages/oauth/Permission/index.tsx
@@ -4,12 +4,6 @@ import type { ProColumns } from '@ant-design/pro-table';
 import { Image } from 'antd';
 import BasePage from '@/components/BasePage';
 import OAuthPermissionService from '@/services/gemini-oauth2/oauth/permission';
-import PermissionLabel from './components/PermissionLabel';
-
-const permissionChinese = {
-  "email": "电子邮箱",
-  "mobile": "手机号码"
-};
 
 class OAuthPermission extends React.Component {
   columns: ProColumns<API.OAuth.OAuthPermissionVO>[] = [
@@ -33,21 +27,21 @@ class OAuthPermission extends React.Component {
       render: (_, record) => {
         const result: any[] = [];
         record.permissions.forEach(permission => {
-          let type = "读取";
+          let element = "";
           if (permission.endsWith('_w')) {
             permission.replace('_w', '');
-            type = "修改";
+            element = "修改";
+          } else {
+            element = "读取";
           }
 
-          let name = "未知";
-          for (const key in permissionChinese) {
-            if (permission.startsWith(key)) {
-              name = permissionChinese[key];
-              break;
-            }
+          if (permission.startsWith("email")) {
+            element += '电子邮箱';
+          } else if (permission.startsWith("mobile")) {
+            element += '手机号码';
           }
 
-          result.push(<PermissionLabel type={type} name={name} />);
+          result.push(element);
         })
         return result;
       }
